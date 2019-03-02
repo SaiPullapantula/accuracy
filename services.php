5a8361bffa50d436f627a1154137fb7434a305be
SaiPullapantula <47951954+SaiPullapantula@users.noreply.github.com>
2019-03-02 00:13:02 -0600

    Update services.php

--- services.php
+++ services.php
@@ -291,7 +291,7 @@
               <div class="well-text text-center">
                 <h2>Welcome To Our Accuracy</h2>
                 <p>
-                  Busuness Lorem ipsum dolor sit amet, consectetur adipiscing elit.luctus est eget congue.
+                 Learn more by subscribing.
                 </p>
                 <div class="subs-feilds">
                   <div class="suscribe-input">
@@ -340,8 +340,8 @@
               <div class="single-icon">
                 <i class="fa fa-envelope-o"></i>
                 <p>
-                  Email: info@example.com<br>
-                  <span>Web: www.example.com</span>
+                  Email: accuracy@gmail.com<br>
+                  <span>Web: https://accuracy-finance.herokuapp.com</span>
                 </p>
               </div>
             </div>
@@ -414,7 +414,6 @@
                   <h2><span>A</span>ccuracy</h2>
                 </div>
 
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis.</p>
                 <div class="footer-icons">
                   <ul>
                     <li>
@@ -439,9 +438,7 @@
             <div class="footer-content">
               <div class="footer-head">
                 <h4>information</h4>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
-                </p>
+                
                 <div class="footer-contacts">
                   <p><span>Tel:</span> +123 456 789</p>
                   <p><span>Email:</span> contact@example.com</p>
