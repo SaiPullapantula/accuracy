f90e59d1fe066b6389f9b87f602749e299a36625
SaiPullapantula <47951954+SaiPullapantula@users.noreply.github.com>
2019-03-06 17:32:31 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -108,10 +108,10 @@
   $('#validateBtn').click(function(){
       var date1 = new Date($('#dateAppointment').val());
       var date2 = new Date();
-      if(date1.getFullYear() > date2.getFullYear()) {
+      if(date1.getFullYear() < date2.getFullYear()) {
          alert("Please enter a valid date.");
       } else {
-          if(date1.getMonth()+1 > date2.getMonth()+1) {
+          if(date1.getMonth()+1 < date2.getMonth()+1) {
               alert("Please enter a valid date.");
           } else {
               alert("Thank you for booking an appoinment!");
