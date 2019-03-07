1c705175521b8772898096929f07b8612b5d47c0
SaiPullapantula <47951954+SaiPullapantula@users.noreply.github.com>
2019-03-06 18:23:18 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -115,7 +115,8 @@
       } else if(date1.getFullYear() == date2.getFullYear()) {
          if(date1.getMonth()+1 < date2.getMonth()+1) {
               alert("Please enter a valid date.");
-          }}
+          }else {
+         alert("Thank you for booking an appointment!")}
       } else {
           alert("Thank you for booking an appoinment!");
       }
